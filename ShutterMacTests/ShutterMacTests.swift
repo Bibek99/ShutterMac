e52805e6354cb440ad5630b42e53415f9a860cbe
Bibek Basyal <bibekbpn@gmail.com>
2025-03-06 11:28:39 +0545

    Initial Commit

---
+++ ShutterMacTests/ShutterMacTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShutterMacTests.swift
+//  ShutterMacTests
+//
+//  Created by Bibek Basyal on 06/03/2025.
+//
+
+import Testing
+@testable import ShutterMac
+
+struct ShutterMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
